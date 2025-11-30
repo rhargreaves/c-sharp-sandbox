936e6c220fff62e16ad71eb8ce09bd28a8a782eb
Robert Hargreaves <kiyouta@gmail.com>
2025-11-30 15:11:34 +0000

    test: consume memory endpoint returns 200

---
+++ CSharpSandbox.Test/ProgramTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace CSharpSandbox.Test;
+
+public class ProgramTest
+{
+    [Fact]
+    public async Task Returns_200()
+    {
+        var app = new WebApplicationFactory<Program>();
+        var client = app.CreateClient();
+
+        var response = await client.GetAsync("/api/consume_memory");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
--- CSharpSandbox/Program.cs
+++ CSharpSandbox/Program.cs
@@ -1,8 +1,26 @@
 ﻿using Microsoft.AspNetCore.Builder;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace CSharpSandbox;
 
-app.MapGet("/", () => "Hello World!");
+public class Program
+{
+    private static readonly Dictionary<string, string> BigDict = new();
 
-app.Run();
+    static async Task<string> RootGetHandler()
+    {
+        for (int i = 0; i < 1_000_000; i++)
+        {
+            BigDict.Add(i.ToString(), i.ToString());
+        }
+
+        return await Task.FromResult($"Memory usage grows >:) Dict size is: {BigDict.Count}");
+    }
+
+    static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+        app.MapGet("/api/consume_memory", RootGetHandler);
+        app.Run();
+    }
+}
